68c9426f5ab708a056b99d2e47fc2362cf4b4938
haoyunlong <hyl19881203@sina.cn>
2022-01-17 20:02:49 +0800

    增加整数拆分代码

--- LeetCodeMaster/dynamic_processing/go/main.go
+++ LeetCodeMaster/dynamic_processing/go/main.go
@@ -156,6 +156,33 @@ func diffWays(route [][]int, m,n int) int {
 
 }
 
+/*
+* 整数拆分
+给定一个正整数 n，将其拆分为至少两个正整数的和，并使这些整数的乘积最大化。 返回你可以获得的最大乘积。
+示例 1: 输入: 2 输出: 1
+\解释: 2 = 1 + 1, 1 × 1 = 1。
+示例 2: 输入: 10 输出: 36 解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36。 说明: 你可以假设 n 不小于 2 且不大于 58。
+*/
+
+/*
+解题思路
+* dp[i]含义：整数拆分后的道的最大乘积
+* 递推公式: dp[i] = max(i * [j - i], dp[i - j] * j)
+* dp初始化：dp[0]和dp[1]没有意义，从dp[2]开始初始化，dp[2] = 1
+*/
+
+func IntegerBreak(n int) int {
+	dp := make([]int, n+1)
+	dp[2] = 1
+	for i := 3; i <= n; i++ {
+		for j := 1; j < i-1; j++ {
+			dp[i] = max(dp[i], max(i*(i-j), dp[i-j]*j))
+		}
+	}
+
+	return dp[n]
+}
+
 func main() {
 	result := fib(10)
 	fmt.Printf("result %d\n", result)
