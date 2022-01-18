823cf5f41bb65986f8f00bb31041b5c8553b7c13
haoyunlong <hyl19881203@sina.cn>
2022-01-18 13:19:22 +0800

    增加石头拆分代码

--- LeetCodeMaster/dynamic_processing/go/main.go
+++ LeetCodeMaster/dynamic_processing/go/main.go
@@ -328,6 +328,38 @@ func CanPartition(nums []int) bool {
 	return false
 }
 
+/*
+* 最后一块石头的重量
+有一堆石头，每块石头的重量都是正整数。
+每一回合，从中选出任意两块石头，然后将它们一起粉碎。假设石头的重量分别为 x 和 y，且 x <= y。那么粉碎的可能结果如下：
+如果 x == y，那么两块石头都会被完全粉碎； 如果 x != y，那么重量为 x 的石头将会完全粉碎，而重量为 y 的石头新重量为 y-x。 最后，最多只会剩下一块石头。返回此石头最小的可能重量。如果没有石头剩下，就返回 0。
+示例： 输入：[2,7,4,1,8,1] 输出：1 解释： 组合 2 和 4，得到 2，所以数组转化为 [2,7,1,8,1]， 组合 7 和 8，得到 1，所以数组转化为 [2,1,1,1]， 组合 2 和 1，得到 1，所以数组转化为 [1,1,1]， 组合 1 和 1，得到 0，所以数组转化为 [1]，这就是最优值。
+
+* 解题思路
+怎样粉碎后得到的重量最小？最好两部分之和为总重量的一半。这样粉碎后得到的重量最小。
+* dp[j] 表示容量为j的背包能装下石头的重量。j的最大值为全部石头总重量的一半，向下取整
+* 递推公式 dp[j] = max(dp[j], dp[j - stone[i]] + stone[i])
+* dp初始化，全部初始化为0
+*/
+
+func lastStoneWeightII(stones []int) int {
+	var sum int
+	for _, v := range stones {
+		sum += v
+	}
+
+	target := sum / 2
+	dp := make([]int, target + 1)
+	
+	for i := 0; i < len(stones); i++ {
+		for j := target; j >= stones[i]; j-- {
+			dp[j] = max(dp[j], dp[j-stones[i]] + stone[i])
+		}
+	}
+
+	return sum - dp[target] - dp[target]
+}
+
 func main() {
 	//result := fib(10)
 	//fmt.Printf("result %d\n", result)
